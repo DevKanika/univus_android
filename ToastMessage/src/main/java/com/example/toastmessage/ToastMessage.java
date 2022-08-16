b5a9003fa5293577448cdd17208191bec8f37e8d
DevKanika <kan16.gweca15@gmail.com>
2022-08-16 18:05:15 +0530

    init

--- ToastMessage/src/main/java/com/example/toastmessage/ToastMessage.java
+++ ToastMessage/src/main/java/com/example/toastmessage/ToastMessage.java
@@ -6,7 +6,7 @@ import android.widget.Toast;
 
 public class ToastMessage {
 
-        public static void toast(Context c, String message) {
+    public static void toast(Context c, String message) {
             Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
         }
 
